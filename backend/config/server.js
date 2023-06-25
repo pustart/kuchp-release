bb66ffd8c1808eae8ea1f9292eddd597d3455a24
pustart <polaris9955@gmail.com>
2023-06-25 17:07:49 +0300

    fix: update strapi server config

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,11 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  proxy: true,
+  url: env('PUBLIC_URL'),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'http://localhost/strapi/'),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
